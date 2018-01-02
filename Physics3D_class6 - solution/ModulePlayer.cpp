3ba3680633c2a7fcaba8e0c7c97b47ef7503af56
DavidTheMaaster <davidthegaamer@gmail.com>
2018-01-03 00:58:31 +0100

    Some changes that didnt comit

--- Physics3D_class6 - solution/ModulePlayer.cpp
+++ Physics3D_class6 - solution/ModulePlayer.cpp
@@ -19,70 +19,8 @@ bool ModulePlayer::Start()
 {
 	LOG("Loading player");
 
-	VehicleInfo car;
+	CreateCar();
 
-	// Car properties ----------------------------------------
-	car.chassis_size.Set(2, 2, 4);
-	car.chassis_offset.Set(0, 1.5, 0);
-	car.mass = 500.0f;
-	car.suspensionStiffness = 15.88f;
-	car.suspensionCompression = 0.83f;
-	car.suspensionDamping = 0.88f;
-	car.maxSuspensionTravelCm = 1000.0f;
-	car.frictionSlip = 50.5;
-	car.maxSuspensionForce = 6000.0f;
-
-	// Wheel properties ---------------------------------------
-	float connection_height = 1.2f;
-	float wheel_radius = 0.6f;
-	float wheel_width = 0.5f;
-	float suspensionRestLength = 1.2f;
-
-	// Don't change anything below this line ------------------
-
-	float half_width = car.chassis_size.x*0.5f;
-	float half_length = car.chassis_size.z*0.5f;
-	
-	vec3 direction(0,-1,0);
-	vec3 axis(-1,0,0);
-	
-	car.num_wheels = 4;
-	car.wheels = new Wheel[4];
-
-	// FRONT-LEFT ------------------------
-	car.wheels[0].connection.Set(half_width - 0.3f * wheel_width, connection_height, half_length - wheel_radius);
-	// FRONT-RIGHT ------------------------
-	car.wheels[1].connection.Set(-half_width + 0.3f * wheel_width, connection_height, half_length - wheel_radius);
-	// REAR-LEFT ------------------------
-	car.wheels[2].connection.Set(half_width - 0.3f * wheel_width, connection_height, -half_length + wheel_radius);
-	// REAR-RIGHT ------------------------
-	car.wheels[3].connection.Set(-half_width + 0.3f * wheel_width, connection_height, -half_length + wheel_radius);
-	
-	for (int i = 0; i < car.num_wheels; i++)
-	{
-		if (i == 0 || i == 1)
-		{
-			car.wheels[i].front = true;
-			car.wheels[i].drive = true;
-			car.wheels[i].steering = true;
-		}
-		else
-		{
-			car.wheels[i].front = false;
-			car.wheels[i].drive = false;
-			car.wheels[i].steering = false;
-		}
-		car.wheels[i].direction = direction;
-		car.wheels[i].axis = axis;
-		car.wheels[i].suspensionRestLength = suspensionRestLength;
-		car.wheels[i].radius = wheel_radius;
-		car.wheels[i].width = wheel_width;
-		car.wheels[i].brake = true;
-	}
-
-	vehicle = App->physics->AddVehicle(car);
-	vehicle->SetPos(0, 12, 10);
-	
 	return true;
 }
 
@@ -129,7 +67,7 @@ update_status ModulePlayer::Update(float dt)
 	
 	char title[80];
 	sprintf_s(title, "%.1f Km/h", vehicle->GetKmh());
-	App->window->SetTitle(title);
+	//App->window->SetTitle(title);
 
 	if (dead == true)
 	{
@@ -143,14 +81,92 @@ update_status ModulePlayer::Update(float dt)
 
 void ModulePlayer::Respawn()
 {
+	if (timer == 0)
+	{
+		vehicle->SetPos(0, 1, 10);
+		vehicle->SetTransform(&matrix);
+	}
+
 	if (timer < 100)
+	{
 		App->player->vehicle->Brake(BRAKE_POWER);
+		timer++;
+	}
 	else
 	{
-		timer = 0;
 		dead = false;
+		timer = 0;
 	}
-	timer++;
+
+	
+
 }
 
+void ModulePlayer::CreateCar()
+{
+	VehicleInfo car;
+	// Car properties ----------------------------------------
+	car.chassis_size.Set(2, 2, 4);
+	car.chassis_offset.Set(0, 1.5, 0);
+	car.mass = 500.0f;
+	car.suspensionStiffness = 15.88f;
+	car.suspensionCompression = 0.83f;
+	car.suspensionDamping = 0.88f;
+	car.maxSuspensionTravelCm = 1000.0f;
+	car.frictionSlip = 50.5;
+	car.maxSuspensionForce = 6000.0f;
 
+	// Wheel properties ---------------------------------------
+	float connection_height = 1.2f;
+	float wheel_radius = 0.6f;
+	float wheel_width = 1;
+	float suspensionRestLength = 0.5f;
+
+	// Don't change anything below this line ------------------
+
+	float half_width = car.chassis_size.x*0.5f;
+	float half_length = car.chassis_size.z*0.5f;
+
+	vec3 direction(0, -1, 0);
+	vec3 axis(-1, 0, 0);
+
+	car.num_wheels = 4;
+	car.wheels = new Wheel[4];
+
+	// FRONT-LEFT ------------------------
+	car.wheels[0].connection.Set(half_width - 0.3f * wheel_width, connection_height, half_length - wheel_radius);
+	// FRONT-RIGHT ------------------------
+	car.wheels[1].connection.Set(-half_width + 0.3f * wheel_width, connection_height, half_length - wheel_radius);
+	// REAR-LEFT ------------------------
+	car.wheels[2].connection.Set(half_width - 0.3f * wheel_width, connection_height, -half_length + wheel_radius);
+	// REAR-RIGHT ------------------------
+	car.wheels[3].connection.Set(-half_width + 0.3f * wheel_width, connection_height, -half_length + wheel_radius);
+
+	for (int i = 0; i < car.num_wheels; i++)
+	{
+		if (i == 0 || i == 1)
+		{
+			car.wheels[i].front = true;
+			car.wheels[i].drive = true;
+			car.wheels[i].steering = true;
+		}
+		else
+		{
+			car.wheels[i].front = false;
+			car.wheels[i].drive = false;
+			car.wheels[i].steering = false;
+		}
+		car.wheels[i].direction = direction;
+		car.wheels[i].axis = axis;
+		car.wheels[i].suspensionRestLength = suspensionRestLength;
+		car.wheels[i].radius = wheel_radius;
+		car.wheels[i].width = wheel_width;
+		car.wheels[i].brake = true;
+	}
+
+	
+	vehicle = App->physics->AddVehicle(car);
+	vehicle->SetPos(0, 1, 10);
+	vehicle->GetTransform(&matrix);
+
+}
--- Physics3D_class6 - solution/ModulePlayer.h
+++ Physics3D_class6 - solution/ModulePlayer.h
@@ -3,7 +3,7 @@
 #include "Globals.h"
 #include "p2Point.h"
 
-struct PhysVehicle3D;
+
 
 #define MAX_ACCELERATION 1000.0f
 #define TURN_DEGREES 15.0f * DEGTORAD
@@ -20,14 +20,16 @@ public:
 	bool CleanUp();
 
 	void Respawn();
+	void CreateCar();
+
 
 public:
 
 	PhysVehicle3D* vehicle = nullptr;
+	mat4x4 matrix;
 	float turn;
 	float acceleration;
 	float brake;
-
 
 	bool dead = false;
 	int timer = 0;
