8bde65bc8a8922c90e09fb173c584b34afd28010
JoanValiente <joan.valiente.lorite@gmail.com>
2018-01-02 19:49:18 +0100

    asdasa

--- Physics3D_class6 - solution/ModuleSceneIntro.h
+++ Physics3D_class6 - solution/ModuleSceneIntro.h
@@ -35,6 +35,4 @@ public:
 	p2List<Cube> walls;
 	p2List<PhysBody3D*> walls_pb;
 
-	Cube floor1, floor2, floor3, floor4, floor5, floor6, floor7, floor8, floor9, floor10, floor11, floor12, floor13, floor14, floor15, floor16, floor17, floor18, floor19, floor20, floor21, floor22, floor23, floor24, floor25, floor26;
-	PhysBody3D *floor1_pb, *floor2_pb, *floor3_pb, *floor4_pb, *floor5_pb, *floor6_pb, *floor7_pb, *floor8_pb, *floor9_pb, *floor10_pb, *floor11_pb, *floor12_pb, *floor13_pb, *floor14_pb, *floor15_pb, *floor16_pb, *floor17_pb, *floor18_pb, *floor19_pb, *floor20_pb, *floor21_pb, *floor22_pb, *floor23_pb, *floor24_pb, *floor25_pb, *floor26_pb;
 };
